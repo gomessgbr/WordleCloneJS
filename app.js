829808c96d54b70456990cc131a028eef28dd1f6
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-02-15 12:02:13 -0300

    style's input elemens and add on screen

--- app.js
+++ app.js
@@ -43,8 +43,16 @@ const guessRows = [
 
 guessRows.forEach((guessRow, guessRowIndex) => {
   const rowElement = document.createElement("div");
-  rowElement.setAttribute("id", "guessRTow-" + guessRowIndex);
-
+  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement("div");
+    tileElement.setAttribute(
+      "id",
+      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
+    );
+    tileElement.classList.add("tile");
+    rowElement.append(tileElement);
+  });
   tileDisplay.append(rowElement);
 });
 
